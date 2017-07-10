9f9f917ce2601b31d8da0012873f216472a6a999
etnlGD <etnlGD@gmail.com>
2017-07-10 22:50:14 +0800

    设置为static

--- tools/configure-win-vs2015-x86.bat
+++ tools/configure-win-vs2015-x86.bat
@@ -5,5 +5,5 @@ set ASMJIT_BUILD_DIR="build_vs2015_x86"
 
 mkdir ..\%ASMJIT_BUILD_DIR%
 cd ..\%ASMJIT_BUILD_DIR%
-cmake .. -G"Visual Studio 14" -DASMJIT_BUILD_TEST=1
+cmake .. -G"Visual Studio 14" -DASMJIT_BUILD_TEST=1 -DASMJIT_STATIC=1
 cd %ASMJIT_CURRENT_DIR%
